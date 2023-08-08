7b3047bba47cc60f73592a0cd728017d349c3da6
maria <mariaeduardamaffezzolli@gmail.com>
2023-08-08 19:04:45 -0300

    telas

--- ifscarona/src/main/java/visao/Inicio.java
+++ ifscarona/src/main/java/visao/Inicio.java
@@ -22,8 +22,8 @@ public class Inicio extends JFrame {
 	private JButton btnCadastrar;
 	private JLabel lblBemvindoAo;
 	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_4;
 	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
 
 	/**
 	 * Launch the application.
@@ -57,45 +57,55 @@ public class Inicio extends JFrame {
 		btnLogar.setBackground(new Color(255, 251, 233));
 		btnLogar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				new Login().setVisible(true);
+				this.dispose();
+
 			}
+
+			private void dispose() {
+				setVisible(false);
+			
+			}
+			
 		});
 		
-		lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\IFSCARONA\\ifscarona\\src\\main\\java\\assets\\decoracao2.1.png"));
-		lblNewLabel_3.setBounds(-48, 669, 403, 517);
-		contentPane.add(lblNewLabel_3);
-		
-		lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\IFSCARONA\\ifscarona\\src\\main\\java\\assets\\decoracao.png"));
-		lblNewLabel_2.setBounds(1667, -139, 403, 517);
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/b378852d-354d-4cd5-9896-a0888a6e0b86.png"));
+		lblNewLabel_2.setBounds(1367, -14, 548, 492);
 		contentPane.add(lblNewLabel_2);
-		btnLogar.setBounds(807, 550, 110, 23);
+		
+		lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/dunso.png"));
+		lblNewLabel_4.setBounds(-19, -131, 800, 1169);
+		contentPane.add(lblNewLabel_4);
+		btnLogar.setBounds(813, 591, 110, 23);
 		contentPane.add(btnLogar);
 		
 		btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.setBackground(new Color(255, 251, 233));
-		btnCadastrar.setBounds(993, 550, 110, 23);
+		btnCadastrar.setBounds(1000, 591, 110, 23);
 		contentPane.add(btnCadastrar);
 		
 		JLabel lblNewLabel = new JLabel("IFSCarona");
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 49));
-		lblNewLabel.setBounds(831, 340, 311, 112);
+		lblNewLabel.setBounds(824, 385, 311, 112);
 		contentPane.add(lblNewLabel);
 		
 
 		lblBemvindoAo = new JLabel("Bem-Vindo ao");
 		lblBemvindoAo.setFont(new Font("Dialog", Font.BOLD, 20));
-		lblBemvindoAo.setBounds(944, 335, 217, 51);
+		lblBemvindoAo.setBounds(944, 376, 217, 51);
 		contentPane.add(lblBemvindoAo);
 		
 		lblImage = new JLabel("");
-		lblImage.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\IFSCARONA\\ifscarona\\src\\main\\java\\assets\\car.png"));
-		lblImage.setBounds(862, 102, 337, 260);
+		lblImage.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/car.png"));
+		lblImage.setBounds(867, 83, 337, 260);
 		contentPane.add(lblImage);
 		
 		lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\IFSCARONA\\ifscarona\\src\\main\\java\\assets\\trás-removebg-preview.png"));
-		lblNewLabel_1.setBounds(777, 210, 384, 454);
+		lblNewLabel_1.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/dd400250-652b-4da0-9bcc-ecb003be729e.png"));
+		lblNewLabel_1.setBounds(748, 203, 559, 600);
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
--- ifscarona/src/main/java/visao/Login.java
+++ ifscarona/src/main/java/visao/Login.java
@@ -4,19 +4,30 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.border.LineBorder;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JTextField;
 import javax.swing.JFormattedTextField;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField txtEmail;
+	private JPasswordField txtSenha;
 
 	/**
 	 * Launch the application.
@@ -42,19 +53,29 @@ public class Login extends JFrame {
 		setBounds(100, 100, 1940, 1119);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(154, 203, 159));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(new Color(255, 251, 233), 2, true));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel ifacarona = new JLabel("IFSCarona");
+		ifacarona.setFont(new Font("Dialog", Font.BOLD, 53));
+		ifacarona.setBounds(26, 372, 327, 107);
+		contentPane.add(ifacarona);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/icons8-senha-60.png"));
+		lblNewLabel_4.setBounds(876, 575, 60, 60);
+		contentPane.add(lblNewLabel_4);
+		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\IFSCARONA\\ifscarona\\src\\main\\java\\assets\\icons8-nova-mensagem-60.png"));
+		lblNewLabel.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/icons8-nova-mensagem-60.png"));
 		lblNewLabel.setBounds(876, 417, 60, 45);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\certo.png"));
-		lblNewLabel_3.setBounds(0, -197, 455, 1650);
+		lblNewLabel_3.setIcon(new ImageIcon("/home/maria/Área de Trabalho/IFSCARONA/ifscarona/src/main/java/assets/fundoClaro.png"));
+		lblNewLabel_3.setBounds(0, -239, 413, 1650);
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_2 = new JLabel("E-mail institucional");
@@ -62,18 +83,78 @@ public class Login extends JFrame {
 		lblNewLabel_2.setBounds(950, 392, 122, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		textField = new JTextField();
-		textField.setBackground(new Color(255, 251, 233));
-		textField.setToolTipText("");
-		textField.setBounds(946, 417, 336, 45);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtEmail = new JTextField();
+		txtEmail.setBackground(new Color(255, 251, 233));
+		txtEmail.setToolTipText("");
+		txtEmail.setBounds(946, 417, 336, 45);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
 		
 
 		
 		JLabel lblNewLabel_1 = new JLabel("Login");
 		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 53));
-		lblNewLabel_1.setBounds(1030, 41, 327, 107);
+		lblNewLabel_1.setBounds(1012, 154, 327, 107);
 		contentPane.add(lblNewLabel_1);
+		
+		JButton btnLogar = new JButton("LOGAR");
+		btnLogar.setBorder(new LineBorder(new Color(244, 234, 213), 4, true));
+		btnLogar.setBackground(new Color(255, 251, 233));
+		btnLogar.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				String senha = String.valueOf(txtSenha.getPassword());
+
+				if (txtEmail.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "O email deve ser inserido!");
+				} else if (senha.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "A senha deve ser inserida!");
+				} 
+				new Inicio().setVisible(true);
+				this.dispose();
+
+			}
+
+			private void dispose() {
+				setVisible(false);
+			}
+		});
+		btnLogar.setBounds(1012, 796, 162, 45);
+		contentPane.add(btnLogar);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Dialog", Font.PLAIN, 13));
+		lblSenha.setBounds(950, 561, 122, 14);
+		contentPane.add(lblSenha);
+		
+		txtSenha = new JPasswordField();		
+		txtSenha.setToolTipText("");
+		txtSenha.setColumns(10);
+		txtSenha.setBackground(new Color(255, 251, 233));
+		txtSenha.setBounds(954, 587, 336, 45);
+		contentPane.add(txtSenha);
+		
+		JLabel lblCad = new JLabel("Não possui cadastro?");
+		lblCad.setFont(new Font("Dialog", Font.PLAIN, 13));
+		lblCad.setBounds(964, 653, 178, 14);
+		contentPane.add(lblCad);
+		
+		JLabel lblLinkAqui = new JLabel("Clique aqui");
+		lblLinkAqui.setFont(new Font("Dialog", Font.PLAIN, 13));
+		lblLinkAqui.setBounds(1116, 653, 178, 14);
+		lblLinkAqui.setForeground(Color.BLUE);
+		lblLinkAqui.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		contentPane.add(lblLinkAqui);
+
+		lblLinkAqui.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		      
+		        CadastroUsuario cadastroFrame = new CadastroUsuario();
+		        cadastroFrame.setVisible(true);
+		        Login.this.dispose(); 
+		    }
+		});
+		
 	}
 }
