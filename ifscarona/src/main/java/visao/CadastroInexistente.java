b322a2703bee106ff73f298d70fcca19608f21fa
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-09-05 08:22:47 -0300

    Telas de erro

---
+++ ifscarona/src/main/java/visao/CadastroInexistente.java
@@ -0,0 +1,62 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Font;
+
+public class CadastroInexistente extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastroInexistente frame = new CadastroInexistente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastroInexistente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 451, 234);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(159, 203, 154));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAviso = new JLabel("");
+		lblAviso.setIcon(new ImageIcon("C:\\Users\\Aluno\\Downloads\\Aviso.png"));
+		lblAviso.setBounds(33, 28, 68, 66);
+		contentPane.add(lblAviso);
+		
+		JLabel lblCadastroInexistente = new JLabel("Cadastro Inexistente!");
+		lblCadastroInexistente.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblCadastroInexistente.setBounds(150, 54, 138, 14);
+		contentPane.add(lblCadastroInexistente);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setBackground(new Color(255, 251, 233));
+		btnNewButton.setBounds(177, 123, 84, 23);
+		contentPane.add(btnNewButton);
+	}
+}
