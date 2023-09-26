55187690ad1056955a689a1419a1d8caaf896f94
Gabriele <gabriele.k2006@aluno.ifsc.edu.br>
2023-09-26 10:00:08 -0300

    tela cadastro erro

---
+++ ifscarona/src/main/java/visao/CadastroErroVeiculo.java
@@ -0,0 +1,76 @@
+package visao;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class CadastroErroVeiculo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastroErroVeiculo frame = new CadastroErroVeiculo();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastroErroVeiculo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 451, 234);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(159, 203, 154));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAviso = new JLabel("");
+		lblAviso.setIcon(new ImageIcon(CadastroErroVeiculo.class.getResource("/assets/alerta.png")));
+		lblAviso.setBounds(45, 45, 48, 48);
+		contentPane.add(lblAviso);
+		
+		JLabel lblCadastroErro = new JLabel("Erro ao cadastrar pessoa!");
+		lblCadastroErro.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblCadastroErro.setBounds(135, 55, 176, 18);
+		contentPane.add(lblCadastroErro);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setBorder(null);
+		btnNewButton.setFont(new Font("Nirmala UI", Font.PLAIN, 13));
+		btnNewButton.setBackground(new Color(255, 251, 233));
+		btnNewButton.setBounds(177, 123, 84, 31);
+		contentPane.add(btnNewButton);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                // Fecha a janela atual
+	                dispose();
+	            }
+	        });
+	}
+
+}
