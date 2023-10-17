6a24d0b17ac3ee8b7896df3b65ca7eedbbd3d534
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-10-17 09:36:00 -0300

    teste unitário

--- ifscarona/src/test/java/teste/CaronaDAOTest.java
+++ ifscarona/src/test/java/teste/CaronaDAOTest.java
@@ -1,12 +1,42 @@
 package teste;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+import controle.CaronaDAO;
+import modelo.Carona;
+import modelo.Pessoa;
+
 public class CaronaDAOTest {
-
+	
+	// TESTE UNITARIO = TESTAR CADA UNIDADE DO CODIGO
+	// annotation
 	@Test
-	public void testMetodoInserir() {
-
+	public void testCaastrarCarona() {
+		
+		Carona carona = new Carona();
+		carona.setIdCarona(null);
+		carona.setMotorista(null);
+		carona.setPassageiro(null);
+		carona.setTrajeto(null);
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(null);
+		pessoa.setCpf(null);
+		pessoa.setDataNasc(null);
+		pessoa.setEmail(null);
+		pessoa.setSenha(null);
+		pessoa.setSobrenome(null);
+		
+		carona.setPassageiro(pessoa);
+		
+		CaronaDAO cDAO = CaronaDAO.getInstancia();
+		Boolean sucesso = cDAO.cadastrarCarona(carona);
+		
+		assertEquals(true, sucesso);
+		
+	
 	}
 
 }
