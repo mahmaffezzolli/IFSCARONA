31c1fd26139ecba7d0069a9a7a7411a3e2aea4ba
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-10-24 07:59:17 -0300

    caronaDAO

--- ifscarona/src/test/java/teste/CaronaDAOTest.java
+++ ifscarona/src/test/java/teste/CaronaDAOTest.java
@@ -2,11 +2,16 @@ package teste;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import controle.CaronaDAO;
+import controle.PessoaDAO;
 import modelo.Carona;
 import modelo.Pessoa;
+import modelo.Trajeto;
+import modelo.Veiculo;
 
 public class CaronaDAOTest {
 	
@@ -15,39 +20,53 @@ public class CaronaDAOTest {
 	@Test
 	public void testCaastrarCarona() {
 		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Letícia");
+		pessoa.setSobrenome("Lima");
+		pessoa.setCpf("78912309811");
+		pessoa.setDataNasc(LocalDate.of(2005, 12, 28));
+		pessoa.setEmail("leticia.lc2005@gamil.com");
+		pessoa.setSenha("legal321");
+		
+		Trajeto trajeto = new Trajeto();
+		trajeto.setIdTrajeto(null);
+		trajeto.setOrigem("Gaspar");
+		trajeto.setDestino("Blumenau");
+		
+		Veiculo veiculo = new Veiculo();
+		veiculo.setIdVeiculo(null);
+		veiculo.setCor("Preto");
+		veiculo.setMarca("Cherry");
+		veiculo.setModelo("SUV");
+		veiculo.setMotorista(pessoa);
+		veiculo.setPlaca("12345678");
+		
 		Carona carona = new Carona();
 		carona.setIdCarona(null);
-		carona.setMotorista(null);
-		carona.setPassageiro(null);
-		carona.setTrajeto(null);
-		
-		Pessoa pessoa = new Pessoa();
-		pessoa.setNome(null);
-		pessoa.setCpf(null);
-		pessoa.setDataNasc(null);
-		pessoa.setEmail(null);
-		pessoa.setSenha(null);
-		pessoa.setSobrenome(null);
+		carona.setMotorista(pessoa);
+		carona.setPassageiro(pessoa);
+		carona.setTrajeto(trajeto);
+		carona.setVeiculo(veiculo);
+		carona.setData(LocalDate.of(2023, 24, 10));
+		carona.setHorario(null);
+		carona.setPassageiro(pessoa);
 		
 		//setou pessoa da carona
-		//carona.setPassageiro(pessoa);
-		
+		carona.setPassageiro(pessoa);
 		CaronaDAO cDAO = CaronaDAO.getInstancia();
 		Boolean sucesso = cDAO.cadastrarCarona(carona);
 		
 		assertEquals(true, sucesso);
-		
 	
 	}
 	
-//	@Test
-//	public void testCadastrarCaronaErro() {
-//		Pessoa pessoa = null;
-//
-//		PessoaDAO pDAO = PessoaDAO.getInstancia();
-//		Boolean erro = pDAO.cadastrarPessoa(pessoa);
-//
-//		assertEquals(false, erro);
-//	}
+	@Test
+	public void testCadastrarCaronaErro() {
+		Pessoa pessoa = null;
 
+	    PessoaDAO pDAO = PessoaDAO.getInstancia();
+		Boolean erro = pDAO.cadastrarPessoa(pessoa);
+
+		assertEquals(false, erro);
+	}
 }
