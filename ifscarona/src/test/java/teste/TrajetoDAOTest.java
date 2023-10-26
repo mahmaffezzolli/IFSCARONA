024891e80caacd197c0925fbb90adb90886e39d4
Gabriele <gabriele.k2006@aluno.ifsc.edu.br>
2023-10-26 12:10:21 -0300

    testes atualizar deletar cadastrar

--- ifscarona/src/test/java/teste/TrajetoDAOTest.java
+++ ifscarona/src/test/java/teste/TrajetoDAOTest.java
@@ -2,15 +2,11 @@ package teste;
 
 import static org.junit.Assert.assertEquals;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-
-import controle.PessoaDAO;
 import controle.TrajetoDAO;
-import modelo.Pessoa;
 import modelo.Trajeto;
-import modelo.Veiculo;
 
 public class TrajetoDAOTest {
 	@Test
@@ -18,26 +14,8 @@ public class TrajetoDAOTest {
 
 		Trajeto trajeto = new Trajeto();
 		trajeto.setDestino("blumenau");
-		trajeto.setIdTrajeto(123);
 		trajeto.setOrigem("Ifsc");
-		
-		Veiculo veiculo = new Veiculo();
-		veiculo.setCor(null);
-		veiculo.setMotorista(null);
-		veiculo.setMarca(null);
-		veiculo.setModelo(null);
-		veiculo.setMotorista(null);
-		veiculo.setPlaca(null);
-		
-		Pessoa pessoa = new Pessoa();
-		pessoa.setNome("Letícia");
-		pessoa.setCpf("88881154048");
-		pessoa.setDataNasc(LocalDate.of(2006, 2, 18));
-		pessoa.setEmail("gabriele@email.com");
-		pessoa.setSenha("123456789");
-		pessoa.setSobrenome("Bratkhufhjhjh");
-		
-		
+		trajeto.setIdTrajeto(123);
 
 		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
 		Boolean sucesso = tDAO.cadastrarTrajeto(trajeto);
@@ -47,11 +25,56 @@ public class TrajetoDAOTest {
 
 	@Test
 	public void testCadastrarTrajetoErro() {
-	Trajeto trajeto = null;
+		Trajeto trajeto = null;
 
-	TrajetoDAO tDAO = TrajetoDAO.getInstancia();
-	Boolean erro = tDAO.cadastrarTrajeto(trajeto);
-		assertEquals(false, erro);	}
+		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
+		Boolean erro = tDAO.cadastrarTrajeto(trajeto);
+		assertEquals(false, erro);
+	}
+
+	@Test
+	public void testAlterarTrajetoSucesso() {
+
+		Trajeto trajeto = new Trajeto();
+		trajeto.setDestino("Gaspar");
+		trajeto.setOrigem("IFSC");
+		trajeto.setIdTrajeto(123);
+
+		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
+		Boolean sucesso = tDAO.alterarTrajeto(trajeto);
+		assertEquals(true, sucesso);
+
+	}
+
+	@Test
+	public void testAlterarTrajetoErro() {
+		Trajeto trajeto = null;
+
+		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
+		Boolean erro = tDAO.alterarTrajeto(trajeto);
+		assertEquals(false, erro);
+	}
+	@Test
+	public void testDeleteTrajetoSucesso() {
+		Trajeto trajeto = new Trajeto();
+		trajeto.setDestino("Blumenau");
+		trajeto.setOrigem("IFSC");
+
+		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
+		Boolean sucesso = tDAO.deletarTrajeto(trajeto);
+		assertEquals(true, sucesso);
+
+	}
+	
+	
+	@Test
+	public void testDeletarTrajetoErro() {
+		Trajeto trajeto = null;
+
+		TrajetoDAO tDAO = TrajetoDAO.getInstancia();
+		Boolean erro = tDAO.deletarTrajeto(trajeto);
+		assertEquals(false, erro);
+	}
+	
 	
 }
-
